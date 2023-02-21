aa40e8ed9c33bdfb0793e80f8e45ade3e70c642d
Geovane Borba <geovanne@live.com>
2023-02-21 20:03:22 -0300

    feat: wrapper transaction component with transaction provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'styled-components'
+import { TransactionProvider } from './contexts/TransactionsContext'
+import { Transaction } from './pages/Transactions'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>initial setup</div>
+      <TransactionProvider>
+        <Transaction />
+      </TransactionProvider>
       <GlobalStyles />
     </ThemeProvider>
   )
